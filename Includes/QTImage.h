a906f8040448118ed43f346f752e0185a35b2b45
Zer0Tools <albert_borisov_2003@mail.ru>
2023-12-21 19:20:31 +0300

    update

--- Includes/QTImage.h
+++ Includes/QTImage.h
@@ -5,6 +5,21 @@
 #include <ctype.h>
 #include "../Includes/BMPImage.h"
 
+#ifdef QTIMAGE_EXPORTS
+  #define QTIMAGE_API __declspec(dllexport)
+#else
+  #define QTIMAGE_API __declspec(dllimport)
+#endif
+
+#define QTIMAGECALL __cdecl
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+
+
 typedef struct qtimage_t
 {
     uint32_t width;
@@ -16,7 +31,7 @@ typedef struct qtimage_t
 
 typedef struct
 {
-    BMPImage* bmp;
+    char* filename;
     uint8_t maxDepth;  
     void (*encoding_fragment_params_fill_func)(QuadTreeNode*);
     void (*encoding_parent_color_blend_func)(QuadTreeNode*);
@@ -36,16 +51,36 @@ typedef struct
     uint8_t (*encoding_child_megre_func) (QuadTreeNode*);
 } QTImage_Encode_r_params;
 
+typedef struct 
+{
+    double maxErrorH;  
+    double minS;
+    double maxS;
+    double minV;
+    double maxV;
+    double averageH;
+    double averageS;    
+    double averageV;
+
+} QTImage_Encoding_Fragment_params;
+
+static double HueErrorAbs(double average, double val);
+
+static void QTImage_Encoding_Fragment_params_fill_func(QuadTreeNode* node);
+
+static void QTImage_Encoding_Parent_Color_Blend_func(QuadTreeNode* node);
+
+static uint8_t QTImage_Encoding_Merge_func(QuadTreeNode* node);
 
 QTImage* QTImage_Ctor();
 void QTImage_Dctor(QTImage** inst);
-void QTImage_Serialize(QTImage* inst, char* filepath);
-QTImage* QTImage_Deserialize(char* filepath);
-
 size_t QTImage_AllocSize(QTImage* inst);
 
-QTImage* QTImage_Encode(QTImage_Encode_params params);
-void QTImage_Decode(QTImage* qtImage, BMPImage** bmp);
+QTIMAGE_API void __cdecl QTImage_Serialize(QTImage* inst, char* filepath);
+QTIMAGE_API QTImage* __cdecl QTImage_Deserialize(char* filepath);
+QTIMAGE_API QTImage* __cdecl QTImage_Encode(QTImage_Encode_params params);
+QTIMAGE_API void __cdecl QTImage_Decode(QTImage* qtImage, char* filename);
+
 
 
 
@@ -54,4 +89,12 @@ static ColorRGB _calculateFragmentColor(BMPImage* bmp, uint32_t startX, uint32_t
 static void _decode(BMPImage* bmp, QuadTreeNode* treeNode, uint8_t depth, uint8_t requireDepth, 
                     unsigned int x, unsigned int y);
 static QuadTreeNode* _encode(QTImage_Encode_r_params params);
+
+#ifdef __cplusplus
+}
 #endif
+
+
+#endif
+
+
